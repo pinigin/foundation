1ff632a3c6cab9ca72b9efcd3bc323001e65faa4
wixl <wixldev@gmail.com>
2020-08-30 19:23:52 +0300

    more logs

--- core.py
+++ core.py
@@ -6,14 +6,19 @@ from src.foundation.singleton.serviceManager import ServiceManager
 from src.foundation.singleton.singleton import Singleton
 from src.foundation.tasks.taskManager import TaskManager
 from src.foundation.util import file, thread
+from src.foundation.util.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class Core(Singleton):
 	def singleton_init(self):
+		logger.info("starting init of core")
 		file.init_folders()
 		self.task_manager = TaskManager()
 		CoreInitializationManager.initialize()
 		ServiceManager()
+		logger.info("finished init of core")
 
 	def start(self):
 		thread.run_in_new_thread(True, "task_loop", self.task_loop)
